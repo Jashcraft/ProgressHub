63916ea7429a803e59fa19a5b4e171860546d9b7
Rich Arce <raarce118@gmail.com>
2022-05-24 22:03:15 -0700

    testing server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,26 +16,6 @@ const resolvers = {
     }
   },
 
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      //const token = signToken(user);
-
-      //return { token, user };
-      
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-     
+}
 
 module.exports = resolvers;
