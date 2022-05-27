70fbc577db6ae91fdc1fd8f047e3744069b7a9f6
MagnificentRoast <94555704+MagnificentRoast@users.noreply.github.com>
2022-05-27 11:50:15 -0700

    removed duplicate margin value from form.jsx

--- client/src/components/Hero/Form.jsx
+++ client/src/components/Hero/Form.jsx
@@ -9,7 +9,7 @@ const Form = () => {
   return (
     <>
       <Paper elevation={6} sx={{ width: "80%", display: "flex", flexDirection: 'column', backgroundColor: "#024e76", color: "#fff", borderRadius: "50px", borderColor: "#fce138", borderStyle: "solid", marginTop: "65px" }}>
-        <Typography variant='h4' sx={{margin: "auto", width: "100%", textAlign: "center", margin: "15px 0px", color: "#fce138", fontWeight: "bold"}}>Join Us Today!</Typography>
+        <Typography variant='h4' sx={{width: "100%", textAlign: "center", margin: "15px 0px", color: "#fce138", fontWeight: "bold"}}>Join Us Today!</Typography>
         {formComponents.map(component => (
           <TextField id={component.toLowerCase()} label={component} variant="outlined" sx={{ width: "80%", ml: "90px", mb: "10px", borderRadius: "50px", backgroundColor: "#fff" }} key={component} />
         ))}
