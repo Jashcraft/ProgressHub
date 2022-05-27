25617805ef2bb4c3122211100fc3f4c7a707010f
MagnificentRoast <94555704+MagnificentRoast@users.noreply.github.com>
2022-05-27 11:48:07 -0700

    Worked on the form and about us sections to add polish for the desktop site

--- client/src/components/Hero/Form.jsx
+++ client/src/components/Hero/Form.jsx
@@ -8,16 +8,16 @@ const formComponents = ["Name", "Email", "Password", "City"]
 const Form = () => {
   return (
     <>
-      <Paper elevation={6} sx={{ width: "80%", display: "flex", flexDirection: 'column', backgroundColor: "#024e76", color: "#fff", borderRadius: "50px", borderColor: "#fce138", borderStyle: "solid" }}>
+      <Paper elevation={6} sx={{ width: "80%", display: "flex", flexDirection: 'column', backgroundColor: "#024e76", color: "#fff", borderRadius: "50px", borderColor: "#fce138", borderStyle: "solid", marginTop: "65px" }}>
         <Typography variant='h4' sx={{margin: "auto", width: "100%", textAlign: "center", margin: "15px 0px", color: "#fce138", fontWeight: "bold"}}>Join Us Today!</Typography>
         {formComponents.map(component => (
-          <TextField id={component.toLowerCase()} label={component} variant="outlined" sx={{ width: "80%", ml: "10px", mb: "10px", backgroundColor: "#fff" }} key={component} />
+          <TextField id={component.toLowerCase()} label={component} variant="outlined" sx={{ width: "80%", ml: "90px", mb: "10px", borderRadius: "50px", backgroundColor: "#fff" }} key={component} />
         ))}
         <FormGroup>
 {/* This switch will determine whether a new user is a trainer or a client */}
-          <FormControlLabel control={<Switch />} label="Are you a Workout Leader?" sx={{ml: "10px", mb: "10px"}}></FormControlLabel>
+          <FormControlLabel control={<Switch />} label="Are you a Workout Leader?" sx={{ml: "10px", mb: "10px", justifyContent: "center", marginTop: "15px"}}></FormControlLabel>
         </FormGroup>
-        <Button sx={{color: "#00A36C", fontWeight: "bolder"}}>Register</Button>
+        <Button sx={{color: "#00A36C", fontWeight: "bolder", marginBottom: "20px"}}>Register</Button>
       </Paper>
     </>
   )
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -9,11 +9,11 @@ const Hero = () => {
       <Grid item xs={6}>
         <Grid display="flex" justifyContent="center">
           <Grid item>
-            <Typography variant='h3' align='center' sx={{color: "#fce138", fontWeight: "bolder"}}>
+            <Typography variant='h3' align='center' sx={{color: "#fce138", fontWeight: "bolder", marginTop: "140px"}}>
               Our Mission
             </Typography>
             {/* This section is the company's 'What Do we Do' statement. */}
-            <Typography align='center' sx={{color: "#fce138", fontWeight: "bold"}}>
+            <Typography align='center' sx={{color: "#fce138", fontWeight: "bold", marginTop: "70px"}}>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur aliquid veniam distinctio minus ipsa optio asperiores quia doloribus expedita dolor nesciunt porro, impedit voluptatibus recusandae maiores sunt nemo accusantium consequatur?
             </Typography>
           </Grid>
